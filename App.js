8c2e16a3b206b6623315b4f361fcc5768b26f865
menthy-wu <wumengxuan0728@gmail.com>
2023-03-31 00:49:44 -0700

    add eslint, prettier, husky

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import React from 'react';
 
+// eslint-disable-next-line require-jsdoc
 export default function App() {
   return (
     <View>
-      <Text className="bg-red-500">
-        Open up App.js to start working on your app!
-      </Text>
+      <Text className="bg-red-500">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./components/homescreen/**/*.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,jsx,ts,tsx}",
+    './App.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './components/homescreen/**/*.{js,jsx,ts,tsx}',
+    './screens/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
