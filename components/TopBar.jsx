44039c6f889eb2a3e43fb1a740a0db040ef1cf17
menthy-wu <wumengxuan0728@gmail.com>
2023-04-13 20:45:05 -0700

    format

--- components/TopBar.jsx
+++ components/TopBar.jsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 
 const TopBar = () => {
   return (
     <View>
       <Text>TopBar</Text>
     </View>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
