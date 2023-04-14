000c513fe73ebc0f50e67fc7862127d088e5a5f0
menthy-wu <wumengxuan0728@gmail.com>
2023-04-13 20:38:35 -0700

    add files

---
+++ screens/loding.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const loding = () => {
+  return (
+    <View>
+      <Text>loding</Text>
+    </View>
+  );
+};
+
+export default loding;
