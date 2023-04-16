f0d40515604d30aa7c6248c1ac0f407119b15de4
Divyank Shah <divyank.shah.2016@gmail.com>
2023-04-16 01:15:27 -0700

    fix aspect ratio

--- src/screens/loading.js
+++ src/screens/loading.js
@@ -4,7 +4,11 @@ import React from 'react';
 const loading = () => {
   return (
     <View className="h-full w-full bg-rmate-blue flex justify-center items-center">
-      <Image source={require('../../assets/UCRlogo.svg')} className="w-32 h-32" />
+      <Image
+        source={require('../../assets/UCRlogo.svg')}
+        className="w-32 h-32"
+        resizeMode="contain"
+      />
     </View>
   );
 };
