a9a5a9fa8fd7ae8731b1af4ced55ece0b3cad30e
Sean Quiambao <slquiambao03@gmail.com>
2023-04-18 16:07:18 -0700

    Bug Screen

--- src/components/Bug.jsx
+++ src/components/Bug.jsx
@@ -1,4 +1,14 @@
-import { View, Text, TouchableOpacity, Image, Alert, SafeAreaView, TextInput, StyleSheet, Pressable } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Alert,
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+  Pressable,
+} from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
@@ -6,50 +16,44 @@ import React from 'react';
 const Bug = () => {
   const navigation = useNavigation();
   return (
-    <SafeAreaView className='bg-[#DEE7F5] w-full h-full'>
-      <View className='flex-row w-full p-3'>
-        <TouchableOpacity onPress = {() => navigation.navigate('Profile')}>
-          <Feather name='arrow-left' size={24} color='rmate-black'/>
+    <SafeAreaView className="bg-[#DEE7F5] w-full h-full">
+      <View className="flex-row w-full p-3">
+        <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+          <Feather name="arrow-left" size={24} color="rmate-black" />
         </TouchableOpacity>
-        <Image 
-          source={require('../../assets/bug.svg')}
-          className='w-7 h-7 p-3'
-        />
+        <Image source={require('../../assets/bug.svg')} className="w-7 h-7 p-3" />
       </View>
 
       {/* Texts */}
 
-      <View className='w-full p-6'>
-        <Text 
-          className='text-black text-[32px] font-bold'
+      <View className="w-full p-6">
+        <Text
+          className="text-black text-[32px] font-bold"
           style={{ fontFamily: 'Poppins_600SemiBold' }}
-        > 
-          Thank you for catching this bug! 
+        >
+          Thank you for catching this bug!
         </Text>
 
         <Text
-          className='text-black text-[15px] font-regular mt-3'
+          className="text-black text-[15px] font-regular mt-3"
           style={{ fontFamily: 'Poppins_400Regular' }}
         >
           Please describe the bug below and we will try to fix it ASAP!
         </Text>
-
-        
       </View>
 
-      <View className='flex-column w-full h-[65vh] p-6'>
+      <View className="flex-column w-full h-[65vh] p-6">
         <TextInput
           multiline
-          style = {styles.input}
-          placeholder='Description'
-          placeholderTextColor='#6F6D6D'
+          style={styles.input}
+          placeholder="Description"
+          placeholderTextColor="#6F6D6D"
         />
 
         <Pressable onPress={() => Alert.alert('Submitted!')} style={styles.appButtonContainer}>
           <Text style={styles.appButtonText}>Submit</Text>
         </Pressable>
       </View>
-
     </SafeAreaView>
   );
 };
@@ -61,7 +65,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     borderRadius: 6,
     padding: 10,
-    flex: 1
+    flex: 1,
   },
   appButtonContainer: {
     alignSelf: 'flex-end',
@@ -72,7 +76,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFB81C',
     borderRadius: 23.5,
     paddingVertical: 10,
-    paddingHorizontal: 12
+    paddingHorizontal: 12,
   },
   appButtonText: {
     fontSize: 20,
@@ -80,8 +84,8 @@ const styles = StyleSheet.create({
     color: 'black',
     fontWeight: 'bold',
     alignSelf: 'center',
-    textTransform: 'uppercase'
-  }
+    textTransform: 'uppercase',
+  },
 });
 
 export default Bug;
