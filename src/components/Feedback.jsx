63b3315c6d81719ee98419c5a53791e42200deb4
Falak Tulsi <falaktulsi@gmail.com>
2023-04-18 21:55:54 -0700

    Ran npm format

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -4,29 +4,29 @@ import { useNavigation } from '@react-navigation/native';
 import { AntDesign } from '@expo/vector-icons';
 import React from 'react';
 
-
 const Feedback = () => {
   const navigation = useNavigation();
   return (
-    <View className="w-full flex justify-center items-center"> 
+    <View className="w-full flex justify-center items-center">
       <View className="bg-rmate-lightblue w-full h-[80vh] p-3">
         <TouchableOpacity className="" onPress={() => navigation.navigate('Profile')}>
           <AntDesign name="arrowleft" size={24} color="black" />
         </TouchableOpacity>
         <View className="w-5/6 flex items-center justify-center h-2/7">
-        <Text
+          <Text
             className="text-rmate-black text-3xl font-bold my-4"
-            style={{ fontFamily: 'Poppins_400Regular'}}
+            style={{ fontFamily: 'Poppins_400Regular' }}
           >
             Feel free to leave us some feedback!
           </Text>
           <Text
             className="text-rmate-black text-3xl my-4"
-            style={{ fontFamily: 'Poppins_400Regular', fontSize: 15, f}}
+            style={{ fontFamily: 'Poppins_400Regular', fontSize: 15, f }}
           >
-            The R&apos;Mate Team is always striving to improve and we appreciate any and all feedback!
+            The R&apos;Mate Team is always striving to improve and we appreciate any and all
+            feedback!
           </Text>
-          </View>
+        </View>
       </View>
     </View>
   );
