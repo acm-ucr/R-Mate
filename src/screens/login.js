c957b5844e6315bba1e98b5f6d6c0a6a812c1e47
Zach Green <zachgreen@Zachary-Greens-MacBook-Pro.local>
2023-04-20 15:38:50 -0700

    Moved the 'Login' text to the upper-left corner of the border

--- src/screens/login.js
+++ src/screens/login.js
@@ -8,7 +8,7 @@ const LoginScreen = () => {
   return (
     <View style={{ flex: 1, borderColor: 'rgba(255, 255, 255, 0.5)', borderWidth: 20, borderRadius: 5 }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#00274C' }}>
-        <Text>Login</Text>
+        <Text style={{ position: 'absolute', top: -17, left: 0 }}>Login</Text>
         <TouchableOpacity
           style={{padding: 10, backgroundColor: '#00274C' }}
           onPress={() => navigation.navigate('BottomNavigator')}
