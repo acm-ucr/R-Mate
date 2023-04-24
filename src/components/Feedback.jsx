f5811670f6265c584029547b09f0f3f41480f395
Falak Tulsi <falaktulsi@gmail.com>
2023-04-24 12:51:35 -0700

    Store user feedback and output to console

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,10 +1,20 @@
-import { View, Text, SafeAreaView, Image, TextInput, Pressable, Alert } from 'react-native';
+import { View, Text, SafeAreaView, Image, TextInput, Pressable } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
-import React from 'react';
+import { React, useState } from 'react';
 
 const Feedback = () => {
+  const [feedback, setFeedback] = useState('');
+
+  const handleTyping = (event) => {
+    setFeedback(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    console.log('Feedback: ' + feedback);
+  };
+
   const navigation = useNavigation();
   return (
     <SafeAreaView className="bg-rmate-lightblue min-w-[100%] min-h-[100%]">
@@ -26,12 +36,13 @@ const Feedback = () => {
         </View>
         <View className="flex flex-column gap-y-5 w-full h-[65vh]">
           <TextInput
+            onChange={handleTyping}
             multiline
             placeholder="Feedback"
             placeholderTextColor="#6F6D6D"
             className="flex-1 font-poppins-400 h-450 bg-rmate-white rounded-md p-3"
           />
-          <Pressable style={{ alignSelf: 'flex-end' }} onPress={() => Alert.alert('Submitted!')}>
+          <Pressable style={{ alignSelf: 'flex-end' }} onPress={handleSubmit}>
             <Text className="bg-rmate-yellow rounded-full px-4 py-2 w-fit font-poppins-700 text-xl uppercase text-black">
               Submit
             </Text>
