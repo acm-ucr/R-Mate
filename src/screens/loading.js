5c35024b50e2011f5f2b139390fb4f9742b220ab
Divyank Shah <divyank.shah.2016@gmail.com>
2023-04-25 15:54:21 -0700

    add svg support

--- src/screens/loading.js
+++ src/screens/loading.js
@@ -1,14 +1,11 @@
-import { View, Image } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
+import UCRLogo from '../../assets/UCRLogo.svg';
 
 const LoadingScreen = () => {
   return (
     <View className="h-full w-full bg-rmate-blue flex justify-center items-center">
-      {/* <Image
-        source={require('../../assets/UCRlogo.svg')}
-        className="w-32 h-32"
-        resizeMode="contain"
-      /> */}
+      <UCRLogo width={100} height={100} />
     </View>
   );
 };
