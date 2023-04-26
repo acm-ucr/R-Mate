d8b075bca432c190a1136a92245d42a9dcd7cb98
Alamode89 <andreamante89@gmail.com>
2023-04-26 16:47:36 -0700

    Added image

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -7,12 +7,14 @@ const Event = () => {
   return (
     <View>
       <TouchableOpacity onPress={() => navigation.navigate('EventPage')}>
-        <View>
-          <Image source={'../assets/resumeWorkShop.png'} className="w-10"/>
-          <Text className="bg-red-500">Resume Workshop with UBA</Text>
-          <Text>Apr 13th 8 - 9:30PM</Text>
-          <Text className="text-rmate-gray">East Lothian Galley</Text>
-          <Text className="text-rmate-white">details</Text>
+        <View className="flex flex-row bg-red-500">
+          <Image source={require('../../assets/resumeWorkShop.png')} className="w-10"/>
+            <View>
+            <Text>Resume Workshop with UBA</Text>
+            <Text>Apr 13th 8 - 9:30PM</Text>
+            <Text className="text-rmate-gray">East Lothian Galley</Text>
+            <Text className="text-rmate-white">details</Text>
+          </View>
         </View>
       </TouchableOpacity>
     </View>
