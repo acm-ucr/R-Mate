8173f39d27f8cf933343ada858e83379d3f6ee39
Divyank Shah <divyank.shah.2016@gmail.com>
2023-04-27 16:38:08 -0700

    feedback fix

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -19,23 +19,26 @@ const Feedback = () => {
   const navigation = useNavigation();
   return (
     <SafeAreaView className="bg-rmate-lightblue min-w-[100%] min-h-[100%]">
-      <View className="flex-row w-full p-3">
-        <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+      <View className="p-3">
+        <TouchableOpacity
+          className="flex flex-row items-center"
+          onPress={() => navigation.navigate('Profile')}
+        >
           <Feather name="arrow-left" size={24} color="black" />
+          <FeedbackSVG />
         </TouchableOpacity>
-        <FeedbackSVG />
       </View>
-      <View className="flex flex-column gap-y-[5vh] w-full h-full px-5">
-        <View className="flex flex-column gap-y-5">
-          <Text className="text-black text-[32px] font-poppins-700">
+      <View className="flex flex-column w-full h-full px-5 mt-4">
+        <View className="flex flex-column">
+          <Text className="text-black text-3xl font-poppins-700">
             Feel free to leave us some feedback!
           </Text>
-          <Text className="text-black text-[15px] font-poppins-400">
+          <Text className="text-black text-lg font-poppins-400 mt-4">
             The R&apos;Mate Team is always striving to improve and we appreciate any and all
             feedback!
           </Text>
         </View>
-        <View className="flex flex-column gap-y-5 w-full h-[65vh]">
+        <View className="flex flex-column w-full h-[60vh] mt-4">
           <TextInput
             onChange={handleTyping}
             multiline
@@ -43,7 +46,7 @@ const Feedback = () => {
             placeholderTextColor="#6F6D6D"
             className="flex-1 font-poppins-400 h-450 bg-rmate-white rounded-md p-3"
           />
-          <Pressable style={{ alignSelf: 'flex-end' }} onPress={handleSubmit}>
+          <Pressable style={{ alignSelf: 'flex-end' }} onPress={handleSubmit} className="mt-4">
             <Text className="bg-rmate-yellow rounded-full px-4 py-2 w-fit font-poppins-700 text-xl uppercase text-black">
               Submit
             </Text>
