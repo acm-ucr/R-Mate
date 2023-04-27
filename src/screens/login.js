b23a5b209b4c247c7ad4bc5dc93db9f536115859
Zach Green <zachgreen@Zachary-Greens-MacBook-Pro.local>
2023-04-26 21:53:31 -0700

    Finished Login Screen outside of some minor adjustments to be made

--- src/screens/login.js
+++ src/screens/login.js
@@ -2,27 +2,49 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native';
-// import ProfileSVG from '../assets/bellTower.svg';
+import BellTower from '../../assets/bellTower.svg';
+import UCLogo from '../../assets/UCLogo.svg';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
   return (
-    <SafeAreaView className="flex flex-col justify-center items-center">
-      <Text>LoginScreen</Text>
-      <TouchableOpacity
-        className="flex flex-col mr-3"
-        onPress={() => navigation.navigate('BottomNavigator')}
-      >
-        <View className="bg-red-400">
-          <Text className="">Click to Login</Text>
+    <SafeAreaView className="h-full w-full bg-rmate-blue flex flex-col justify-center items-center">
+      <View className="w-full flex flex-row justify-end">
+        <BellTower width={146} height={727} />
+      </View>
+      <View className="absolute top-12 left-10 flex flex-row items-center">
+        <UCLogo width={50} height={50} />
+        <Text className="text-rmate-offwhite text-2xl font-poppins-600 ml-2">RIVERSIDE</Text>
+      </View>
+      <View className="absolute top-40 left-5 flex flex-col">
+        <Text className="text-rmate-offwhite text-3xl font-poppins-400 ml-2">Welcome to</Text>
+        <View className="flex flex-row">
+          <Text className="text-rmate-yellow text-5xl font-poppins-400 ml-2">R</Text>
+          <Text className="text-rmate-offwhite text-5xl font-poppins-400 ml-2">&apos;Mate</Text>
         </View>
-      </TouchableOpacity>
-      <TouchableOpacity
-        className="flex flex-col mr-3"
-        onPress={() => navigation.navigate('LoadingScreen')}
-      >
-        <View className="bg-blue-400">
-          <Text className="">Click to Loading Screen</Text>
+        <View className="flex flex-col flex-wrap">
+          <Text></Text>
+          <Text></Text>
+          <Text></Text>
+          <Text className="text-rmate-offwhite text-lg font-poppins-400 ml-2">
+            The University of
+          </Text>
+          <Text className="text-rmate-offwhite text-lg font-poppins-400 ml-2">
+            California, Riverside's
+          </Text>
+          <Text className="text-rmate-offwhite text-lg font-poppins-400 mt-1 ml-2">
+            online community!
+          </Text>
+        </View>
+      </View>
+      <TouchableOpacity className="absolute bottom-20 left-0 w-full">
+        <View className="flex justify-center items-center flex-row w-full">
+          <TouchableOpacity
+            className="bg-rmate-yellow rounded-full w-9/12 h-16 justify-center items-center"
+            onPress={() => navigation.navigate('BottomNavigator')}
+          >
+            <Text className="text-rmate-blue text-2xl font-poppins-600 ml-2">Sign In</Text>
+          </TouchableOpacity>
         </View>
       </TouchableOpacity>
     </SafeAreaView>
@@ -30,51 +52,3 @@ const LoginScreen = () => {
 };
 
 export default LoginScreen;
-
-{/* <View
-className="h-screen flex"
-style={{ borderColor: 'rgba(255, 255, 255, 0.5)', borderWidth: 20, borderRadius: 5 }}
->
-<View
-  style={{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#003DA5',
-  }}
->
-  <Image
-    source={require('../../assets/bellTower.svg')}
-    className="w-32 h-1/2"
-    resizeMode="contain"
-  />
-   <Image
-    source={require('../../assets/UCLogo.svg')}
-    className="w-32 h-1/2"
-    resizeMode="contain"
-  />
-  <Text style={{ position: 'absolute', top: -17, left: 0 }}>Login</Text>
-  <TouchableOpacity
-    style={{
-      position: 'absolute',
-      top: 700,
-      backgroundColor: '#D3D3D3',
-      padding: 20,
-      borderRadius: 10,
-      shadowColor: '#000',
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
-    }}
-    onPress={() => navigation.navigate('BottomNavigator')}
-  >
-    <View>
-      <Text style={{ color: '#00274C', fontWeight: 'bold', fontSize: 18 }}>Sign In</Text>
-    </View>
-  </TouchableOpacity>
-</View>
-</View> */}
