9ef9dfac2b11d00e09c0f24d1ae819af1d166e0b
Alamode89 <andreamante89@gmail.com>
2023-04-26 18:17:12 -0700

    Even more formatting on the events

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -5,15 +5,15 @@ import { useNavigation } from '@react-navigation/native';
 const Event = () => {
   const navigation = useNavigation();
   return (
-    <View className="rounded-xl bg-red-500 p-1">
+    <View className="rounded-xl bg-rmate-lightblue p-2 w-5/6">
       <TouchableOpacity onPress={() => navigation.navigate('EventPage')}>
-        <View className="flex flex-row ml-2">
-          <Image source={require('../../assets/resumeWorkShop.png')} className="w-20 h-20 " />
-          <View className="pl-5">
+        <View className="flex flex-row ml-2 w-full">
+          <Image source={require('../../assets/resumeWorkShop.png')} className="w-20 h-20" />
+          <View className="pl-2">
             <Text className="font-poppins-600">Resume Workshop with UBA</Text>
-            <Text className="text-rmate-gray font-poppins-300">Apr 13th 8 - 9:30PM</Text>
-            <Text className="text-rmate-gray">East Lothian Galley</Text>
-            <View className="flex items-center">
+            <Text className="text-rmate-gray font-poppins-400">Apr 13th 8 - 9:30PM</Text>
+            <Text className="text-rmate-gray font-poppins-400">East Lothian Galley</Text>
+            <View className="flex py-1 items-end">
               <View className="bg-rmate-blue rounded-full px-3">
                 <Text className="text-rmate-white font-poppins-300 my-0.5">details</Text>
               </View>
