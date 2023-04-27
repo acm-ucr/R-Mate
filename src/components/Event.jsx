725ef6b0172aebb3a484d90b0589a03c92fdce2d
Alamode89 <andreamante89@gmail.com>
2023-04-26 17:42:45 -0700

    Text formatting

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -5,15 +5,17 @@ import { useNavigation } from '@react-navigation/native';
 const Event = () => {
   const navigation = useNavigation();
   return (
-    <View>
+    <View className="rounded-xl bg-red-500">
       <TouchableOpacity onPress={() => navigation.navigate('EventPage')}>
-        <View className="flex flex-row bg-red-500">
-          <Image source={require('../../assets/resumeWorkShop.png')} className="w-10"/>
-            <View>
-            <Text>Resume Workshop with UBA</Text>
-            <Text>Apr 13th 8 - 9:30PM</Text>
+        <View className="flex flex-row">
+          <Image source={require('../../assets/resumeWorkShop.png')} className="flex w-10 h-10 items-center" />
+          <View className="">
+            <Text className="font-poppins-600">Resume Workshop with UBA</Text>
+            <Text className="text-rmate-gray">Apr 13th 8 - 9:30PM</Text>
             <Text className="text-rmate-gray">East Lothian Galley</Text>
-            <Text className="text-rmate-white">details</Text>
+            <View className="flex bg-rmate-blue rounded-xl items-center">
+              <Text className="text-rmate-white">details</Text>
+            </View>
           </View>
         </View>
       </TouchableOpacity>
