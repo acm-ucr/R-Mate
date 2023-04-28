7aeb25ba0843ecd07b0d2b976d1fddc8b30a80e2
Rama-Chunduri <130624276+Rama-Chunduri@users.noreply.github.com>
2023-04-27 21:12:10 -0700

    Update home.js

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import React from 'react';
 import Welcome from '../components/Welcome';
 import ResourcesCard from '../components/ResourcesCard';
