c4b4430aa9e35a029ffb4af2d52061de8d9321a1
Sean Quiambao <slquiambao03@gmail.com>
2023-04-27 21:09:05 -0700

    Attempted Logout confirm

--- src/screens/logout.js
+++ src/screens/logout.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-
 const LogoutScreen = () => {
-    const navigation = useNavigation();
-    return(
-        <SafeAreaView className="bg-rmate-lightblue w-[100%] h-[100%]">
-        </SafeAreaView>
-    );
+  const navigation = useNavigation();
+  return <SafeAreaView className="bg-rmate-lightblue w-[100%] h-[100%]"></SafeAreaView>;
 };
+
+export default LogoutScreen;
