fca76858675f06638c521f2cc89cbc14bc4b0719
Benedict Ignacio <btmignacio818@gmail.com>
2023-04-27 19:43:58 -0700

    Changed bug style to tailwind utility class

--- src/components/Bug.jsx
+++ src/components/Bug.jsx
@@ -5,7 +5,6 @@ import {
   Alert,
   SafeAreaView,
   TextInput,
-  StyleSheet,
   Pressable,
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
@@ -44,47 +43,54 @@ const Bug = () => {
       <View className="w-full h-[60vh] py-6 px-3">
         <TextInput
           multiline
-          style={styles.input}
+          className="h-full poppins-400 bg-rmate-white rounded-md p-2.5 flex-1"
           placeholder="Description"
           placeholderTextColor="#6F6D6D"
         />
 
-        <Pressable onPress={() => Alert.alert('Submitted!')} style={styles.appButtonContainer}>
-          <Text style={styles.appButtonText}>Submit</Text>
+        <Pressable onPress={() => Alert.alert('Submitted!')} className="self-end w-1/3 bg-rmate-yellow rounded-3xl p-2.5 mt-6 leading-8">
+          <Text className="text-xl poppins-600 text-rmate-black font-bold self-center normalcase">Submit</Text>
         </Pressable>
       </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  input: {
-    height: 450,
-    fontFamily: 'Poppins_400Regular',
-    backgroundColor: 'white',
-    borderRadius: 6,
-    padding: 10,
-    flex: 1,
-  },
-  appButtonContainer: {
-    alignSelf: 'flex-end',
-    marginTop: 25,
-    width: 136,
-    height: 47,
-    elevation: 8,
-    backgroundColor: '#FFB81C',
-    borderRadius: 23.5,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-  },
-  appButtonText: {
-    fontSize: 20,
-    fontFamily: 'Poppins_600SemiBold',
-    color: 'black',
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    textTransform: 'uppercase',
-  },
-});
+// style={styles.appButtonText}
+// style={styles.appButtonContainer}
+// style={styles.input}
+// className="text-xl poppins-600 text-rmate-black font-bold self-center uppercase"
+// className="poppins-400 bg-rmate-white rounded-md p-2.5 flex-1"
+// className="flex-end bg-rmate-yellow rounded-3xl py-2.5 px-3"
+
+// const styles = StyleSheet.create({
+//   input: {
+//     height: 450,
+//     fontFamily: 'Poppins_400Regular',
+//     backgroundColor: 'white',
+//     borderRadius: 6,
+//     padding: 10,
+//     flex: 1,
+//   },
+//   appButtonContainer: {
+//     alignSelf: 'flex-end',
+//     marginTop: 25,
+//     width: 136,
+//     height: 47,
+//     elevation: 8,
+//     backgroundColor: '#FFB81C',
+//     borderRadius: 23.5,
+//     paddingVertical: 10,
+//     paddingHorizontal: 12,
+//   },
+//   appButtonText: {
+//     fontSize: 20,
+//     fontFamily: 'Poppins_600SemiBold',
+//     color: 'black',
+//     fontWeight: 'bold',
+//     alignSelf: 'center',
+//     textTransform: 'uppercase',
+//   },
+// });
 
 export default Bug;
