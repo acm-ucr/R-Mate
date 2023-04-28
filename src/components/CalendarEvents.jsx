a5fde7eae497d549429eae9679b23711d11d60d3
theo <theobfernandez@gmail.com>
2023-04-27 21:02:32 -0700

    Run Prettier

--- src/components/CalendarEvents.jsx
+++ src/components/CalendarEvents.jsx
@@ -3,10 +3,7 @@ import React, { useState } from 'react';
 import Events from './Events';
 import { Calendar, LocaleConfig } from 'react-native-calendars';
 
-
 const CalendarEvents = () => {
-  
-
   LocaleConfig.locales['cstm'] = {
     monthNames: [
       'January',
@@ -24,30 +21,30 @@ const CalendarEvents = () => {
     ],
     dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     dayNamesShort: ['S', 'M', 'T', 'W', 'TH', 'F', 'S'],
-    today: "Today",
+    today: 'Today',
   };
 
   LocaleConfig.defaultLocale = 'cstm';
 
- const getCurrentDate = () => {
-   const date = new Date().getDate();
-   const month = new Date().getMonth() + 1;
-   const year = new Date().getFullYear();
-   if (month < 10) {
-     return year + '-0' + month + '-' + date;
-   }
-   return year + "-" + month + "-" + date; 
- }
+  const getCurrentDate = () => {
+    const date = new Date().getDate();
+    const month = new Date().getMonth() + 1;
+    const year = new Date().getFullYear();
+    if (month < 10) {
+      return year + '-0' + month + '-' + date;
+    }
+    return year + '-' + month + '-' + date;
+  };
 
-const [selected, setSelected] = useState(getCurrentDate());
-const markedDates ={
-  '2023-04-02': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
-  '2023-04-03': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
-  '2023-04-04': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
-  '2023-04-05': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
-  '2023-04-07': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
-  '2023-04-14': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
-};
+  const [selected, setSelected] = useState(getCurrentDate());
+  const markedDates = {
+    '2023-04-02': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
+    '2023-04-03': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
+    '2023-04-04': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
+    '2023-04-05': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
+    '2023-04-07': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
+    '2023-04-14': { marked: true, dotColor: '#FFB81C', activeOpacity: 0 },
+  };
 
   return (
     <View className="w-full h-full bg-rmate-blue">
@@ -56,7 +53,7 @@ const markedDates ={
         onDayPress={(day) => {
           setSelected(day.dateString);
         }}
-        markedDates={{ ...markedDates, [selected]: { selected: true, disableTouchEvent: true, } }}
+        markedDates={{ ...markedDates, [selected]: { selected: true, disableTouchEvent: true } }}
         theme={{
           backgroundColor: '#003DA5',
           calendarBackground: '#003DA5',
