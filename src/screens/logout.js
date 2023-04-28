fe89c869604bf0f5ab230d026a854fc9074bf01d
Sean Quiambao <slquiambao03@gmail.com>
2023-04-27 21:02:17 -0700

    Added logout.js

---
+++ src/screens/logout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+
+const LogoutScreen = () => {
+    const navigation = useNavigation();
+    return(
+        <SafeAreaView className="bg-rmate-lightblue w-[100%] h-[100%]">
+        </SafeAreaView>
+    );
+};
