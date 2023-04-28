fd06553441cc611e42178540fb44a7c2a3863b02
Falak Tulsi <falaktulsi@gmail.com>
2023-04-27 20:41:03 -0700

    Adjusted feedback component sizes

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -18,7 +18,7 @@ const Feedback = () => {
 
   const navigation = useNavigation();
   return (
-    <SafeAreaView className="bg-rmate-lightblue min-w-[100%] min-h-[100%]">
+    <SafeAreaView className="bg-rmate-lightblue w-full h-full">
       <View className="p-3">
         <TouchableOpacity
           className="flex flex-row items-center"
@@ -30,10 +30,10 @@ const Feedback = () => {
       </View>
       <View className="flex flex-column w-full h-full px-5 mt-4">
         <View className="flex flex-column">
-          <Text className="text-black text-3xl font-poppins-700">
+          <Text className="text-black text-lg font-poppins-700">
             Feel free to leave us some feedback!
           </Text>
-          <Text className="text-black text-lg font-poppins-400 mt-4">
+          <Text className="text-black text- font-poppins-400 mt-4">
             The R&apos;Mate Team is always striving to improve and we appreciate any and all
             feedback!
           </Text>
@@ -44,12 +44,14 @@ const Feedback = () => {
             multiline
             placeholder="Feedback"
             placeholderTextColor="#6F6D6D"
-            className="flex-1 font-poppins-400 h-450 bg-rmate-white rounded-md p-3"
+            className="flex-1 font-poppins-400 h-400 bg-rmate-white rounded-lg p-3"
           />
           <Pressable style={{ alignSelf: 'flex-end' }} onPress={handleSubmit} className="mt-4">
-            <Text className="bg-rmate-yellow rounded-full px-4 py-2 w-fit font-poppins-700 text-xl uppercase text-black">
-              Submit
-            </Text>
+            <View className="bg-rmate-yellow rounded-full px-4 py-2 w-fit">
+              <Text className="font-poppins-700 text-sm uppercase text-black">
+                Submit
+              </Text>
+            </View>
           </Pressable>
         </View>
       </View>
