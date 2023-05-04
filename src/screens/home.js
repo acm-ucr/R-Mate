5a2ed9d1b6224377958174181d0b475b7bd0e8a3
Lackline <victor2965777@gmail.com>
2023-05-03 23:51:43 -0700

    debugging HomeScreen

--- src/screens/home.js
+++ src/screens/home.js
@@ -2,16 +2,18 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import Welcome from '../components/Welcome';
 import ResourcesCard from '../components/ResourcesCard';
+import HealthSVG from '../../assets/Health.svg';
 
 const HomeScreen = ({ name }) => {
   return (
     <View className="w-full flex flex-row">
-      <Welcome name="Ashley" />
+      <Welcome name="Hailey" />
       <Text>home</Text>
-      <ResourcesCard text="Health" icon={require('../../assets/Health.svg')} />
-      <ResourcesCard text="SRC" icon={require('../../assets/SRC.svg')} />
+      <ResourcesCard text="Health" icon={HealthSVG} />
+      <ResourcesCard text="Stuff" icon={require('../../assets/SRC.svg')} />
     </View>
   );
 };
 
 export default HomeScreen;
+
