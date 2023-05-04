ea6d77aad608454b55ff0dbea2f0671bea9d2853
Sean Quiambao <slquiambao03@gmail.com>
2023-05-03 22:53:26 -0700

    Repositioned button and UCR SVG

--- src/screens/logout.js
+++ src/screens/logout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable, Text, View, SafeAreaView } from 'react-native';
+import { Text, View, SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 import UCRSVG from '../../assets/UCR.svg';
@@ -12,7 +12,7 @@ const LogoutScreen = () => {
     <SafeAreaView className="bg-rmate-lightblue w-full h-full">
       <View className="p-3">
         <TouchableOpacity
-          className="flex flex-row items-center"
+          className="flex-row items-center"
           onPress={() => navigation.navigate('Profile')}
         >
           <Feather name="arrow-left" size={24} color="black" />
@@ -20,20 +20,24 @@ const LogoutScreen = () => {
         </TouchableOpacity>
       </View>
 
-      <View className="w-full h-full p-3">
-        <Text className="text-black text-lg font-poppins-700 mt-4">
-          You sure you want to logout?
+      <View className="ml-5 mt-3">
+        <Text className="text-black text-left text-3xl font-poppins-600 mt-4">
+          You sure you want {'\n'}
+          to logout?
         </Text>
-
+      </View>
+      <View className="w-full h-1/2 pt-3 justify-center items-center">
         <UCRSVG />
-        <Pressable
+      </View>
+      <View className="w-full h-1/2 justify-center">
+        <TouchableOpacity
           onPress={() => navigation.navigate('LoginScreen')}
-          className="self-center w-5/6 bg-rmate-yellow rounded-3xl p-2.5 mt-6 leading-8"
+          className="bg-rmate-yellow rounded-3xl w-5/6 p-2.5 self-center"
         >
-          <Text className="text-xl poppins-600 text-black font-bold self-center normalcase">
+          <Text className="text-2xl poppins-600 text-rmate-black font-bold self-center normalcase">
             Confirm
           </Text>
-        </Pressable>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
