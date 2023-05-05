ac9db32e7a7e3ab309512b12bc655584b4c01c18
Lackline <victor2965777@gmail.com>
2023-05-04 20:50:07 -0700

    HomeScreen temporary layout

--- src/screens/home.js
+++ src/screens/home.js
@@ -7,20 +7,19 @@ import srcSVG from '../../assets/SRC.svg';
 import LibrarySVG from '../../assets/Library.svg';
 import DirectorySVG from '../../assets/phone.svg';
 
-
 const HomeScreen = ({ name }) => {
   return (
     <View className="w-full flex flex-col bg-rmate-lightblue h-full">
       <Welcome name="Hailey" />
       <View className="max-w-screen-lg w-full p-4">
-      <ResourcesCard text="Health" icon={HealthSVG} />
-      <ResourcesCard text="SRC" icon={srcSVG} />
-      <ResourcesCard text="Library" icon={LibrarySVG} />
-      <ResourcesCard text="Directory" icon={DirectorySVG} />
-      <ResourcesCard text="<fillin 1>" icon={DirectorySVG} />
-      <ResourcesCard text="<fillin 2>" icon={DirectorySVG} />
-      <ResourcesCard text="<fillin 3>" icon={DirectorySVG} />
-      <ResourcesCard text="<fillin 4>" icon={DirectorySVG} />
+        <ResourcesCard text="Health" icon={HealthSVG} />
+        <ResourcesCard text="SRC" icon={srcSVG} />
+        <ResourcesCard text="Library" icon={LibrarySVG} />
+        <ResourcesCard text="Directory" icon={DirectorySVG} />
+        <ResourcesCard text="<fillin 1>" icon={DirectorySVG} />
+        <ResourcesCard text="<fillin 2>" icon={DirectorySVG} />
+        <ResourcesCard text="<fillin 3>" icon={DirectorySVG} />
+        <ResourcesCard text="<fillin 4>" icon={DirectorySVG} />
       </View>
     </View>
   );
