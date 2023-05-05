2c4ae06d47cea0a9735d15c8f45196c187c3d285
Lackline <victor2965777@gmail.com>
2023-05-04 20:48:49 -0700

    HomeScreen temporary layout

--- src/components/ResourcesCard.jsx
+++ src/components/ResourcesCard.jsx
@@ -5,9 +5,9 @@ const ResourcesCard = (props) => {
   return (
     <View className="flex items-center">
       <View className="mb-2 bg-white rounded-xl h-16 w-16 flex justify-center items-center">
-        <props.icon width={100} height={100} />
+        <props.icon width={40} height={40} />
       </View>
-      <Text className="text-sm font-poppins-600">{props.text}</Text>
+      <Text className="text-sm font-poppins-600 mb-2">{props.text}</Text>
     </View>
   );
 };
--- src/screens/home.js
+++ src/screens/home.js
@@ -3,12 +3,25 @@ import React from 'react';
 import Welcome from '../components/Welcome';
 import ResourcesCard from '../components/ResourcesCard';
 import HealthSVG from '../../assets/Health.svg';
+import srcSVG from '../../assets/SRC.svg';
+import LibrarySVG from '../../assets/Library.svg';
+import DirectorySVG from '../../assets/phone.svg';
+
 
 const HomeScreen = ({ name }) => {
   return (
-    <View className="w-full flex flex-col bg-blue-500 h-full">
+    <View className="w-full flex flex-col bg-rmate-lightblue h-full">
       <Welcome name="Hailey" />
+      <View className="max-w-screen-lg w-full p-4">
       <ResourcesCard text="Health" icon={HealthSVG} />
+      <ResourcesCard text="SRC" icon={srcSVG} />
+      <ResourcesCard text="Library" icon={LibrarySVG} />
+      <ResourcesCard text="Directory" icon={DirectorySVG} />
+      <ResourcesCard text="<fillin 1>" icon={DirectorySVG} />
+      <ResourcesCard text="<fillin 2>" icon={DirectorySVG} />
+      <ResourcesCard text="<fillin 3>" icon={DirectorySVG} />
+      <ResourcesCard text="<fillin 4>" icon={DirectorySVG} />
+      </View>
     </View>
   );
 };
