66e363b57d448efb21b12563de651f3a5d148111
menthy-wu <wumengxuan0728@gmail.com>
2023-05-04 20:13:03 -0700

    create dropdown

---
+++ src/components/SRCDropdown.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const SRCDropdown = () => {
+  return (
+    <View>
+      <Text>SRCDropdown</Text>
+    </View>
+  );
+};
+
+export default SRCDropdown;
