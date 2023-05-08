93fffd122c97a6f8b20a6b0bb39c997e62594860
Alamode89 <andreamante89@gmail.com>
2023-05-08 05:45:41 -0700

    Created switch based on status

--- src/components/SRCHourButton.jsx
+++ src/components/SRCHourButton.jsx
@@ -2,6 +2,15 @@ import { View, Text } from 'react-native';
 import React from 'react';
 
 const SRCHourButton = ({ status }) => {
+  let style = '';
+  let text = '';
+
+  switch(status) {
+    case 'open':
+      style = 'bg-green-500 text-white';
+      text = 'Open';
+  }
+
   return (
     <View>
       <Text>SRCHourButton</Text>
