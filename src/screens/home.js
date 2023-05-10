9197d621c8494163a20061ba8c5004dc5811de14
Divyank Shah <divyank.shah.2016@gmail.com>
2023-05-10 02:47:25 -0700

    formatting

--- src/screens/home.js
+++ src/screens/home.js
@@ -50,18 +50,18 @@ const HomeScreen = ({ name }) => {
   return (
     <View className="w-full flex flex-col bg-rmate-lightblue h-full">
       <Welcome name="Hailey" />
-        <SafeAreaView className = "flex items-center justify-center">
-          <View className = "w-full p-2">
-            <FlatList
-              className="w-full flex p-2"
-              data={resources}
-              ItemSeparatorComponent={() => <View style={{ height: 5}}/>}
-              numColumns={4}
-              key={4}
-              renderItem={({ item }) => <ResourcesCard text={item.text} icon={item.icon} />}
-            />
-          </View>
-        </SafeAreaView>
+      <SafeAreaView className="flex items-center justify-center">
+        <View className="w-full p-2">
+          <FlatList
+            className="w-full flex p-2"
+            data={resources}
+            ItemSeparatorComponent={() => <View style={{ height: 5 }} />}
+            numColumns={4}
+            key={4}
+            renderItem={({ item }) => <ResourcesCard text={item.text} icon={item.icon} />}
+          />
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
