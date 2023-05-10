442d767ea8c1d2133383d71a8cec4206e881a380
Lackline <victor2965777@gmail.com>
2023-05-09 23:52:14 -0700

    good spacing, height center revise need

--- src/components/ResourcesCard.jsx
+++ src/components/ResourcesCard.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 const ResourcesCard = (props) => {
   return (
     <View className="flex items-center">
-      <View className="mb-2 bg-white rounded-xl h-16 w-16 flex justify-center items-center">
+      <View className="mb-2 ml-5 bg-white rounded-xl h-16 w-16 flex justify-center items-center">
         <props.icon width={40} height={40} />
       </View>
-      <Text className="text-sm font-poppins-600 mb-2">{props.text}</Text>
+      <Text className="text-sm font-poppins-600 mb-2 ml-5">{props.text}</Text>
     </View>
   );
 };
