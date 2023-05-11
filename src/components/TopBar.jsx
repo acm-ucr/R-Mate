2747b6a682f3cca64a4775cbab6b9ae8831106a8
Ricardo Galeano <ricardogaleano@Ricardos-Air-3.home>
2023-05-11 14:24:00 -0700

    Removed unused TouchableOpacity from component

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,15 +1,13 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+// import { TouchableOpacity } from 'react-native';
 
 // eslint-disable-next-line react/prop-types
 const TopBar = () => {
   return (
     <View>
       <Text className="bg-blue-500">UCR LOGO</Text>
-      <TouchableOpacity accessibilityRole="button" onPress={() => {}}>
-        <Text className="bg-red-500">profile</Text>
-      </TouchableOpacity>
+      <Text className="bg-red-500">profile</Text>
     </View>
   );
 };
