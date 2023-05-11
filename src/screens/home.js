af8906c242b377e7f10ac146d73ca4c238289929
menthy-wu <wumengxuan0728@gmail.com>
2023-05-11 01:08:15 -0700

    resources card navigation

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,68 +1,24 @@
-import { View, FlatList, SafeAreaView } from 'react-native';
 import React from 'react';
-import Welcome from '../components/Welcome';
-import DirectorySVG from '../../assets/phone.svg';
-import ResourcesCard from '../components/ResourcesCard';
-import HealthSVG from '../../assets/Health.svg';
-import srcSVG from '../../assets/SRC.svg';
-import LibrarySVG from '../../assets/Library.svg';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../components/Home';
+import SRC from '../components/SRC';
+import Health from '../components/Health';
+import Directory from '../components/Directory';
+import Library from '../components/Library';
 
-const resources = [
-  {
-    text: 'Health',
-    icon: HealthSVG,
-  },
-  {
-    text: 'SRC',
-    icon: srcSVG,
-  },
-  {
-    text: 'Library',
-    icon: LibrarySVG,
-  },
-  {
-    text: 'Directory',
-    icon: DirectorySVG,
-  },
-  {
-    text: 'Add...',
-    icon: HealthSVG,
-  },
-  {
-    text: 'Add...',
-    icon: HealthSVG,
-  },
-  {
-    text: 'Add...',
-    icon: HealthSVG,
-  },
-  {
-    text: 'Add...',
-    icon: HealthSVG,
-  },
-  {
-    text: 'Add...',
-    icon: HealthSVG,
-  },
-];
-
-const HomeScreen = ({ name }) => {
+const screenOptions = {
+  headerShown: false,
+};
+const HomeStack = createNativeStackNavigator();
+const HomeScreen = () => {
   return (
-    <View className="w-full flex flex-col bg-rmate-lightblue h-full">
-      <Welcome name="Hailey" />
-      <SafeAreaView className="flex items-center justify-center">
-        <View className="w-screen pt-3 flex items-center row-center">
-          <FlatList
-            className="w-fit flex pr-4"
-            data={resources}
-            ItemSeparatorComponent={() => <View style={{ height: 5 }} />}
-            numColumns={4}
-            key={4}
-            renderItem={({ item }) => <ResourcesCard text={item.text} icon={item.icon} />}
-          />
-        </View>
-      </SafeAreaView>
-    </View>
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="home" component={Home} options={screenOptions} />
+      <HomeStack.Screen name="src" component={SRC} options={screenOptions} />
+      <HomeStack.Screen name="health" component={Health} options={screenOptions} />
+      <HomeStack.Screen name="library" component={Library} options={screenOptions} />
+      <HomeStack.Screen name="directory" component={Directory} options={screenOptions} />
+    </HomeStack.Navigator>
   );
 };
 
