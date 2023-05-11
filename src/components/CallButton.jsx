beecff7903846cf8bd5ff3782f605677583614c6
menthy-wu <wumengxuan0728@gmail.com>
2023-05-11 00:04:18 -0700

    create call butotn

---
+++ src/components/CallButton.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const CallButton = () => {
+  return (
+    <View>
+      <Text>CallButton</Text>
+    </View>
+  );
+};
+
+export default CallButton;
