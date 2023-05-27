990c0d0b43c477d2edcf366a7e80e22d19c91877
Rama Chunduri <chunduri156@gmail.com>
2023-05-27 14:56:22 -0700

    button

--- src/components/SRCButton.jsx
+++ src/components/SRCButton.jsx
@@ -8,7 +8,7 @@ const SRCButton = () => {
     <View className="w-full flex flex-row">
       <TouchableOpacity className="rounded-xl">
         <Text className="bg-rmate-midblue text-rmate-white font-poppins-400 text-xl pt-2 pl-2 pb-2 pr-2">
-          <Text>Today's Schedule </Text>
+          <Text>Today&apos;s Schedule </Text>
           <AntDesign name="right" size={24} color="white" />
         </Text>
       </TouchableOpacity>
