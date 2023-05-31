306cc60e6b8fd652c8655ef36291222f5ef54c83
menthy-wu <wumengxuan0728@gmail.com>
2023-05-31 12:42:28 -0700

    fix ddrop down

--- src/components/SRCDropdown.jsx
+++ src/components/SRCDropdown.jsx
@@ -1,47 +1,56 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
-import SelectList from "react-native-dropdown-select-list";
 import Icon from "react-native-vector-icons/Ionicons";
 import { useState } from "react";
+import { TouchableOpacity } from "react-native";
 
 const SRCDropdown = () => {
-  const [setSelected] = useState("");
+  const [expand, setExpand] = useState(false);
+  const [selected, setSelected] = useState("Activity Classes");
   const data = [
-    { key: "1", value: "Aquatics" },
-    { key: "2", value: "Competitive Sports" },
-    { key: "3", value: "Cooking Well" },
-    { key: "4", value: "Fitwell" },
-    { key: "5", value: "Open Recreation" },
-    { key: "6", value: "Outdoor Excursions" },
-    { key: "7", value: "Ropes" },
-    { key: "8", value: "Youth Programs" },
+    "Activity Classes",
+    "Aquatics",
+    "Competitive Sports",
+    "Cooking Well",
+    "Fitwell",
+    "Open Recreation",
+    "Outdoor Excursions",
+    "Ropes",
+    "Youth Programs",
   ];
   return (
-    <View>
-      <SelectList
-        data={data}
-        placeholder="Activity Classes"
-        searchPlaceholder=""
-        save="value"
-        arrowicon={<Icon name="md-caret-up" size={25} color={"white"} />}
-        closeicon={<Icon name="md-caret-down" size={25} color={"white"} />}
-        boxStyles={{ backgroundColor: "#003DA5", borderRadius: 14, padding: 0 }}
-        inputStyles={{
-          color: "white",
-          fontFamily: "Poppins_500Medium",
-          fontSize: 20,
-          borderRadius: 20,
+    <View className="z-50">
+      <TouchableOpacity
+        activeOpacity={1}
+        className="justify-between items-center p-3 flex flex-row relative z-50 bg-rmate-blue rounded-lg"
+        onPress={() => {
+          setExpand(!expand);
         }}
-        dropdownStyles={{ backgroundColor: "#5A8BDF" }}
-        dropdownItemStyles={{ padding: 2 }}
-        dropdownTextStyles={{
-          color: "white",
-          fontFamily: "Poppins_500Medium",
-          fontSize: 20,
-        }}
-        setSelected={setSelected}
-        maxHeight={400}
-      />
+      >
+        <Text className=" text-white text-xl font-poppins-400">{selected}</Text>
+        {expand ? (
+          <Icon name="md-caret-down" size={25} color={"white"} />
+        ) : (
+          <Icon name="md-caret-up" size={25} color={"white"} />
+        )}
+      </TouchableOpacity>
+      {expand && (
+        <View className="absolute z-40 bg-rmate-midblue w-full justify-between p-3 rounded-lg pt-16">
+          {data.map((item, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => {
+                setSelected(item);
+                setExpand(false);
+              }}
+            >
+              <Text className="py-1 text-white text-xl font-poppins-400">
+                {item}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      )}
     </View>
   );
 };
