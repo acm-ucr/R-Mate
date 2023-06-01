75b9be3bc6144e60426f6600e437bc8dbd60641d
Alamode89 <andreamante89@gmail.com>
2023-06-01 07:48:50 -0700

    Functions for title of none

--- src/components/FoodHours.jsx
+++ src/components/FoodHours.jsx
@@ -5,7 +5,6 @@ const FoodHours = ({ hours }) => {
   const [activeTab, setActiveTab] = useState("");
 
   const renderTime = (activeTab) => {
-    console.log(activeTab);
     return (
       <View className="w-full bg-rmate-blue">
         {hours.map((timeSlot) =>
@@ -33,20 +32,34 @@ const FoodHours = ({ hours }) => {
 
   return (
     <View>
-      {hours[0].title === "Breakfast/Brunch" && (
-        <View className="flex flex-row">
-          <TouchableOpacity onPress={() => setActiveTab("Breakfast/Brunch")}>
-            <Text>Breakfast/Brunch</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setActiveTab("Lunch")}>
-            <Text>Lunch</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setActiveTab("Dinner")}>
-            <Text>Dinner</Text>
-          </TouchableOpacity>
+      {hours[0].title === "none" && (
+        <View className="w-full bg-rmate-blue">
+          {hours.map((hour, index) => (
+            <View key={index}>
+              <Text>{hour.title}</Text>
+              {hour.hours.map((operationalHours, index) => (
+                <Text key={index}>{operationalHours}</Text>
+              ))}
+            </View>
+          ))}
         </View>
       )}
-      {renderTime(activeTab)}
+      {hours[0].title === "Breakfast/Brunch" && (
+        <View>
+          <View className="flex flex-row">
+            <TouchableOpacity onPress={() => setActiveTab("Breakfast/Brunch")}>
+              <Text>Breakfast/Brunch</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setActiveTab("Lunch")}>
+              <Text>Lunch</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setActiveTab("Dinner")}>
+              <Text>Dinner</Text>
+            </TouchableOpacity>
+          </View>
+          {renderTime(activeTab)}
+        </View>
+      )}
     </View>
   );
 };
