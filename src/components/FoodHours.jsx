386aaa7b3b5cd163db56d61ff86fbd086ea660d8
Alamode89 <andreamante89@gmail.com>
2023-06-01 08:15:44 -0700

    Styles and conditional colors

--- src/components/FoodHours.jsx
+++ src/components/FoodHours.jsx
@@ -2,11 +2,31 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 
 const FoodHours = ({ hours }) => {
-  const [activeTab, setActiveTab] = useState("");
+  const [activeTab, setActiveTab] = useState(hours[0].title);
+
+  let bgColor = "";
+  let textColor = "";
+
+  const colorStyle = () => {
+    if (
+      activeTab === "Breakfast/Brunch" ||
+      activeTab === "none" ||
+      activeTab === "Patio Bar"
+    ) {
+      bgColor = "bg-rmate-lightblue";
+      textColor = "text-rmate-black";
+    } else if (activeTab === "Lunch" || activeTab === "Indoor Dining") {
+      bgColor = "bg-rmate-blue";
+      textColor = "text-rmate-white";
+    } else if (activeTab === "Dinner") {
+      bgColor = "bg-rmate-midblue";
+      textColor = "text-rmate-white";
+    }
+  };
 
   const renderTime = (activeTab) => {
     return (
-      <View className="w-full bg-rmate-blue">
+      <View className={`w-full rounded-xl p-3 ${bgColor}`}>
         {hours.map((timeSlot) =>
           timeSlot.title === activeTab ? (
             <View key={timeSlot}>
@@ -15,10 +35,8 @@ const FoodHours = ({ hours }) => {
                   className="flex flex-row justify-between py-1"
                   key={index}
                 >
-                  <Text className="text-rmate-white font-poppins-600">
-                    {day}
-                  </Text>
-                  <Text className="text-rmate-white font-poppins-600">
+                  <Text className={`${textColor} font-poppins-600`}>{day}</Text>
+                  <Text className={`${textColor} font-poppins-600`}>
                     {timeSlot.time[index]}
                   </Text>
                 </View>
@@ -33,10 +51,9 @@ const FoodHours = ({ hours }) => {
   return (
     <View>
       {hours[0].title === "none" && (
-        <View className="w-full bg-rmate-blue">
+        <View className="w-full bg-rmate-lightblue">
           {hours.map((hour, index) => (
             <View key={index}>
-              <Text>{hour.title}</Text>
               {hour.hours.map((operationalHours, index) => (
                 <Text key={index}>{operationalHours}</Text>
               ))}
@@ -48,13 +65,28 @@ const FoodHours = ({ hours }) => {
         <View>
           <View className="flex flex-row">
             <TouchableOpacity onPress={() => setActiveTab("Breakfast/Brunch")}>
-              <Text>Breakfast/Brunch</Text>
+              <View className="bg-rmate-lightblue border-t-2 border-x-2 rounded-md px-2 py-0">
+                <Text className="font-poppins-500 text-md mx-2">
+                  Breakfast/Brunch
+                </Text>
+                {colorStyle()}
+              </View>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => setActiveTab("Lunch")}>
-              <Text>Lunch</Text>
+              <View className="bg-rmate-blue border-t-2 border-x-2 rounded-md px-2 py-0">
+                <Text className="text-rmate-white font-poppins-500 text-md mx-2">
+                  Lunch
+                </Text>
+                {colorStyle()}
+              </View>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => setActiveTab("Dinner")}>
-              <Text>Dinner</Text>
+              <View className="bg-rmate-midblue border-t-2 border-x-2 rounded-md px-2 py-0">
+                <Text className="text-rmate-white font-poppins-500 text-md mx-2">
+                  Dinner
+                </Text>
+                {colorStyle()}
+              </View>
             </TouchableOpacity>
           </View>
           {renderTime(activeTab)}
